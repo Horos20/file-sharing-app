94aebd919aee6482e4dfb55d190910454a3ab14c
Matis Rammi <themrrammi@gmail.com>
2022-06-28 20:14:29 +0300

    DB connection, DB model, password encryption

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
+require("dotenv").config();
 const multer = require("multer")
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const File = require("./models/File")
 
 const express = require("express")
 const app = express()
 
 const upload = (multer({ dest: "uploads" }))
+
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+const cluster = process.env.CLUSTER;
+const dbname = process.env.DBNAME;
+
+mongoose.connect(`mongodb+srv://${username}:${password}@${cluster}.tpfx3.mongodb.net/${dbname}?retryWrites=true&w=majority`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+    console.log("Connected successfully");
+});
 
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + '/styles'))
@@ -12,8 +33,17 @@ app.get("/", (req, res) => {
     res.render("index")
 })
 
-app.post("/upload", upload.single("file"), (req, res) => {
-    res.send()
+app.post("/upload", upload.single("file"), async (req, res) => {
+    const fileData = {
+        path: req.file.path,
+        originalName: req.file.originalname
+    }
+    if (req.body.password != null && req.body.password !== "") {
+        fileData.password = await bcrypt.hash(req.body.password, 10)
+    }
+
+    const file = await File.create(fileData)
+    res.send(file.originalName)
 })
 
-app.listen (3000)
+app.listen (process.env.PORT)
